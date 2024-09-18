9f1fcd51ca9e1757cb135cf0534ee012c213e538
Andrew <ak36794ca@gmail.com>
2024-09-18 09:15:02 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,12 @@
-const staticCacheName = "static-cache-v06";
+const staticCacheName = "static-cache-v01";
 
 const staticAssets = [
     "./logo192.png",
     "./favicon.ico",
     "./telegram-web-app.js",
-    "./static/js/main.ac25a9fe.js",
+    "./static/js/main.9386a2a6.js",
     "./static/css/main.e6785ae5.css"
 ];
-
 self.addEventListener("install", async event => {
     const cache = await caches.open(staticCacheName);
     await cache.addAll(staticAssets);
@@ -38,7 +37,7 @@ self.addEventListener("fetch", event => {
                 // If not in cache, try to fetch from network
                 return fetch(event.request)
                     .then(networkResponse => {
-                        console.log("!!!!",networkResponse);
+                        console.log("!!!!",JSON.stringify(networkResponse.body));
                         // Check if the response is valid
                         if (!networkResponse.ok) {
                             throw new Error('Network response was not ok');
@@ -48,7 +47,8 @@ self.addEventListener("fetch", event => {
                     .catch(error => {
                         console.error('Fetch failed; returning offline message.', error);
                         // Return a custom message if there's an error
-                        return new Response("No new data available", {
+                        return new Response("No new data available 2020-08-22T13:55:26.769Z", {
+                            data:{dateUpdate:"2020-08-22T13:55:26.769Z"},
                             status: 404,
                             statusText: "Not Found"
                         });
