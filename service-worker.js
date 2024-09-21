716a47cc659bd1d04a1045800b9da417dcf89e7d
Andrew <ak36794ca@gmail.com>
2024-09-21 15:47:12 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const staticCacheName = "static-cache-v01";
+const staticCacheName = "static-cache-v05";
 
 const staticAssets = [
     "./logo192.png",
     "./favicon.ico",
     "./telegram-web-app.js",
-    "./static/js/main.0327d715.js",
-    "./static/css/main.58128328.css"
+    "./static/js/main.633f1388.js",
+    "./static/css/main.a4285f3e.css"
 ]
 self.addEventListener("install", async event => {
     const cache = await caches.open(staticCacheName);
